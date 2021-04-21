f824f7a43b86c7f3ab9a063febfe3b673c323c61
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-04-21 16:12:22 +0200

    user model + saving into mongoose atlas

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,25 @@
-
+const User = require("../models/user");
 
 function post(req,res){
     let name = req.params.name;
 
-    res.json({
-        user: name,
-        message: `made new user ${name}`
-    })
+    if(name != null){
+        let user = new User();
+        user.username = name;
+
+        user.save((err,doc) => {
+
+            if(!err){
+                res.json({
+                    "status": "succes",
+                    "data":{
+                    "message": doc
+                    }
+             })
+        }
+     });
+        
+    }
 }
 
 module.exports.post = post
