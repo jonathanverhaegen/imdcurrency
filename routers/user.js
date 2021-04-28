dd72c1462da6dcb7cd04fe15c88accb19821a724
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-04-28 16:36:38 +0200

    started with new files

--- routers/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/user");
-
-router.post("/:name", userController.post)
-
-
-  module.exports = router;
