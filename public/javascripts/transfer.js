13218da21014deaeefed5cca2f5cb613069486be
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-05-19 21:36:48 +0200

    user can now transfer coins via the receiver mail

---
+++ public/javascripts/transfer.js
@@ -0,0 +1,40 @@
+let sendBtn = document.querySelector('#sendCoins');
+
+sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    let amount = document.querySelector("#amount").value;
+    let receiver = document.querySelector("#receiver").value
+    let reason =  document.querySelector("#reason").value;
+    let message =  document.querySelector("#message").value;
+
+    if(amount > 0){
+        
+        fetch('http://localhost:3000/api/v1/transfers', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({
+            "receiverMail": receiver,
+            "amount": amount,
+            "reason": reason,
+            "text": message
+        })
+        }).then(result =>{
+            return result.json();
+        }).then(json => {
+            
+        })
+
+
+    }else{
+        let feedback = document.querySelector(".alert");
+                feedback.textContent = "please enter a valid number that is higher than 0";
+                feedback.classList.remove("hidden");
+    }
+
+    
+    
+})
