44ae82e41c02842cb685d7bccbae4de37980e22e
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-05-22 14:27:32 +0200

    project is now responsive

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -132,10 +132,7 @@ fetch('http://localhost:3000/api/v1/transfers', {
 })
 
 
-
-
-
-let logoutBtns = document.querySelector("#logout");
+let logout = document.querySelector("#logout");
 
 logout.addEventListener("click", (e) => {
     e.preventDefault();
