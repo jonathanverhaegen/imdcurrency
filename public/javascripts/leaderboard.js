186b8ee89363813d6222251501760594f5f39a41
JakeDarb <stijn.bouckaert2209@icloud.com>
2021-05-26 02:05:47 +0200

    fix[leaderboard]: live update

--- public/javascripts/leaderboard.js
+++ public/javascripts/leaderboard.js
@@ -1,3 +1,8 @@
+// Redirect to login if not logged in
+if(localStorage.getItem('token') === null){
+    window.location.href = "login.html";    
+}
+
 var url = "http://localhost:3000"
 
 primus = Primus.connect(url, {
@@ -8,32 +13,38 @@ primus = Primus.connect(url, {
     }
   });
 
-  primus.on('data', (json)=> {
-      if(json.action === "updateLeaderboard"){
-          addToLeaderboard(json.data);
-      }
-  })
+// Add rows to leaderboard
+let addToLeaderboard = (json) => {
+document.querySelector("#leaderboard").innerHTML = "";
+    $position = 1;
+    json.transfer.forEach((e) => {
+        var record = `<li class="leaderboard__row"><span class="leaderboard__cell">${$position}</span><span class="leaderboard__cell">${e.firstname} ${e.lastname}</span><span class="leaderboard__cell">${e.coins} coins</span></li>`;
+        document.querySelector("#leaderboard").insertAdjacentHTML('beforeend', record);
+        $position++;
+    })
+}
 
-  let addToLeaderboard = (json) => {
-      $position = 1;
-      json.transfer.forEach((e) => {
-          var record = `<li class=""><span>${$position}</span> ${e.firstname} ${e.lastname} <span>${e.coins} coins</span></li>`;
-          document.querySelector("#leaderboard").insertAdjacentHTML('beforeend', record);
-          $position++;
-      })
-  }
+// Load leaderboard data
+let loadLeaderboard = () => {
+    fetch(url+"/api/v1/users/leaderboard", {
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+    }).then(result =>{
+        return result.json();
+    }).then(json => {
+        addToLeaderboard(json);
+    })
+}
 
-  fetch(url+"/api/v1/users/leaderboard", {
-      method: "get",
-      headers: {
-          "Content-Type": "application/json",
-          'Authorization': 'Bearer ' + localStorage.getItem('token')
-      }
-  }).then(result =>{
-      return result.json();
-  }).then(json => {
-      primus.write({
-          "action": "updateLeaderboard",
-          "data": json
-      })
-  })
+// Load leaderboard on transfer
+primus.on('data', (json)=> {
+    if(json.action === "add transfer"){
+        loadLeaderboard();
+    }
+})
+
+// Initial leaderboard load
+loadLeaderboard();
